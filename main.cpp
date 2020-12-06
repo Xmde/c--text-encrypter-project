e68a6b3e364319c4760f08ab1a0df7d998e399d2
django <django@inventobot.com>
2020-12-06 15:56:12 -0800

    workign gui

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <string>
+// wxWidgets "Hello world" Program
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+class MyApp : public wxApp
+{
+public:
+	virtual bool OnInit();
+};
+class MyFrame : public wxFrame
+{
+public:
+	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+private:
+	void OnExit(wxCommandEvent& event);
+	wxDECLARE_EVENT_TABLE();
+};
 
-int main(){
-    //do stuff
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+
+EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+
+wxEND_EVENT_TABLE()
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+	MyFrame* frame = new MyFrame("encrypter", wxPoint(50, 50), wxSize(450, 340));
+	frame->Show(true);
+	return true;
 }
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+	: wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+
